8fd143443b553077233fd17260f0d7b7749b7400
wpcwev <dnakkaunt1337@mail.com>
2025-08-26 01:09:50 +0300

    34543

--- main.py
+++ main.py
@@ -32,16 +32,12 @@ MUTED_PERMS = ChatPermissions(
     can_send_other_messages=False,
     can_add_web_page_previews=False,
     can_change_info=False,
-    can_invite_users=False,
     can_pin_messages=False,
 )
 
 UNMUTED_PERMS = ChatPermissions(
     can_send_messages=True,
     can_send_media_messages=True,
-    can_send_polls=True,
-    can_send_other_messages=True,
-    can_add_web_page_previews=True,
     can_invite_users=True,
     can_pin_messages=False,
     can_change_info=False,
